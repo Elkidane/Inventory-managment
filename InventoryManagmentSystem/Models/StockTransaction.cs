c29ac32ee79aa64a072a47aae22afab08e183cfd
Salah Abdella <abdellasalah9@gmail.com>
2026-06-05 22:14:12 +0300

    Create stock transaction model and database table

---
+++ InventoryManagmentSystem/Models/StockTransaction.cs
@@ -0,0 +1,17 @@
+﻿namespace InventoryManagementSystem.Models
+{
+    public class StockTransaction
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; }
+
+        public string TransactionType { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime TransactionDate { get; set; }
+    }
+}
