c710e512b3ff2bb85c5d75d3726fa4b92b037ea7
Edend12 <edendesalegn4@gmail.com>
2026-06-07 02:12:36 +0300

    implement product listing and CRUD operations

---
+++ InventoryManagmentSystem/Forms/ProductList.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace InventoryManagmentSystem.Forms
+{
+    partial class ProductList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvProducts = new DataGridView();
+            btnLoad = new Button();
+            btnAdd = new Button();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            btnSearch = new Button();
+            txtSearch = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dgvProducts).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvProducts
+            // 
+            dgvProducts.AllowUserToResizeRows = false;
+            dgvProducts.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvProducts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvProducts.Location = new Point(12, 74);
+            dgvProducts.MultiSelect = false;
+            dgvProducts.Name = "dgvProducts";
+            dgvProducts.ReadOnly = true;
+            dgvProducts.RowHeadersVisible = false;
+            dgvProducts.RowHeadersWidth = 62;
+            dgvProducts.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvProducts.Size = new Size(1534, 519);
+            dgvProducts.TabIndex = 0;
+            dgvProducts.CellContentClick += dgvProducts_CellContentClick;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Font = new Font("Segoe UI", 12F);
+            btnLoad.Location = new Point(22, 12);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(195, 56);
+            btnLoad.TabIndex = 1;
+            btnLoad.Text = "Load Products";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Font = new Font("Segoe UI", 12F);
+            btnAdd.Location = new Point(1358, 28);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(154, 40);
+            btnAdd.TabIndex = 2;
+            btnAdd.Text = "Add Product";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += button1_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Font = new Font("Segoe UI", 12F);
+            btnUpdate.Location = new Point(334, 599);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(197, 58);
+            btnUpdate.TabIndex = 3;
+            btnUpdate.Text = "Update Product";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.BackColor = Color.Brown;
+            btnDelete.Font = new Font("Segoe UI", 12F);
+            btnDelete.ForeColor = SystemColors.ButtonHighlight;
+            btnDelete.Location = new Point(699, 599);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(188, 58);
+            btnDelete.TabIndex = 4;
+            btnDelete.Text = "Delete Product";
+            btnDelete.UseVisualStyleBackColor = false;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // btnSearch
+            // 
+            btnSearch.BackColor = Color.MediumTurquoise;
+            btnSearch.Font = new Font("Segoe UI", 12F);
+            btnSearch.Location = new Point(970, 20);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(111, 40);
+            btnSearch.TabIndex = 5;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = false;
+            btnSearch.Click += btnSearch_Click;
+            // 
+            // txtSearch
+            // 
+            txtSearch.BackColor = SystemColors.Control;
+            txtSearch.Location = new Point(630, 25);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(324, 31);
+            txtSearch.TabIndex = 6;
+            // 
+            // ProductList
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonFace;
+            ClientSize = new Size(1550, 703);
+            Controls.Add(txtSearch);
+            Controls.Add(btnSearch);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnAdd);
+            Controls.Add(btnLoad);
+            Controls.Add(dgvProducts);
+            Name = "ProductList";
+            Text = "ProductList";
+            Load += ProductList_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvProducts).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button btnLoad;
+        private Button btnAdd;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private Button btnSearch;
+        private TextBox txtSearch;
+        private DataGridView dgvProducts;
+    }
+}
---
+++ InventoryManagmentSystem/Forms/ProductList.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using InventoryManagementSystem.Data;
+using System.Linq;
+
+namespace InventoryManagmentSystem.Forms
+{
+    public partial class ProductList : Form
+    {
+        public ProductList()
+        {
+            InitializeComponent();
+        }
+
+
+        private void RefreshProducts()
+        {
+            AppDbContext context = new AppDbContext();
+
+            dgvProducts.DataSource = context.Products.ToList();
+        }
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            RefreshProducts();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProductForm form = new ProductForm();
+
+            form.ShowDialog();
+            RefreshProducts();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            AppDbContext context = new AppDbContext();
+
+            string keyword = txtSearch.Text;
+
+            var products = context.Products
+                .Where(p => p.Name.Contains(keyword))
+                .ToList();
+
+            dgvProducts.DataSource = products;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dgvProducts.SelectedRows.Count > 0)
+            {
+                DialogResult result = MessageBox.Show(
+                    "Are you sure you want to delete this product?",
+                    "Confirm Delete",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning);
+
+                if (result == DialogResult.Yes)
+                {
+                    int productId = Convert.ToInt32(
+                        dgvProducts.SelectedRows[0]
+                        .Cells["Id"].Value);
+
+                    using (var context = new AppDbContext())
+                    {
+                        var product =
+                            context.Products.Find(productId);
+
+                        context.Products.Remove(product);
+
+                        context.SaveChanges();
+                    }
+
+                    MessageBox.Show("Product Deleted");
+
+                    RefreshProducts();
+                }
+            }
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (dgvProducts.SelectedRows.Count > 0)
+            {
+                ProductForm form = new ProductForm();
+
+                form.ProductId = Convert.ToInt32(
+                    dgvProducts.SelectedRows[0].Cells["Id"].Value);
+
+                form.txtName.Text =
+                    dgvProducts.SelectedRows[0].Cells["Name"].Value.ToString();
+
+                form.txtCategory.Text =
+                    dgvProducts.SelectedRows[0].Cells["Category"].Value.ToString();
+
+                form.txtPrice.Text =
+                    dgvProducts.SelectedRows[0].Cells["Price"].Value.ToString();
+
+                form.txtSupplier.Text =
+                    dgvProducts.SelectedRows[0].Cells["Supplier"].Value.ToString();
+
+                form.numQuantity.Value =
+                    Convert.ToInt32(
+                        dgvProducts.SelectedRows[0].Cells["Quantity"].Value);
+
+                form.ShowDialog();
+                RefreshProducts();
+            }
+        }
+
+        private void ProductList_Load(object sender, EventArgs e)
+        {
+
+            if (Session.CurrentUser.Role == "Staff")
+            {
+                btnDelete.Visible = false;
+
+
+            }
+            foreach (DataGridViewRow row in dgvProducts.Rows)
+            {
+                int quantity = Convert.ToInt32(
+                    row.Cells["Quantity"].Value);
+
+                if (quantity < 5)
+                {
+                    row.DefaultCellStyle.BackColor = Color.LightCoral;
+                }
+            }
+        }
+
+        private void dgvProducts_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
